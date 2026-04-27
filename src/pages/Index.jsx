87ba3bc91996ffb8cdbef9624c998bff4912b7a7
Syauqi <sauki084@gmail.com>
2026-04-27 10:24:36 +0700

    Data filter feature and useState introduction

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Article from "../components/Article";
 
 function Homepage() {
+  const [search, setSearch] = useState("");
+  const changeSearch = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value);
+  };
   return (
     <>
       <h1>Welcome to the Homepage</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} date={blog.date} tags={blog.tags} />
+      <div>
+        Cari Artikel:{" "}
+        <input type="text" placeholder="Search..." onChange={changeSearch} />
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata "{search}"</small>
+      {posts.map(({ title, date, tags }, index) => (
+        <Article {...{ title, date, tags }} key={index} />
       ))}
     </>
   );
