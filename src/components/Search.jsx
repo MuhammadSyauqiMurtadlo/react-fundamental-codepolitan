733174dc6e15534574f48053c4f7ecfa852fe95a
Syauqi <sauki084@gmail.com>
2026-04-29 06:03:43 +0700

    onClick Methods

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,16 +2,27 @@ import { useState } from "react";
 
 function Search(props) {
   const [search, setSearch] = useState("");
-  const changeSearch = (event) => {
-    setSearch(event.target.value);
-    props.onSearch(event.target.value);
+  const changeSearch = () => {
+    props.onSearch(search);
+  };
+
+  const searchKeyDown = (e) => {
+    if (e.key === "Enter") {
+      changeSearch();
+    }
   };
 
   return (
     <>
       <div>
         Cari Artikel:{" "}
-        <input type="text" placeholder="Search..." onChange={changeSearch} />
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchKeyDown}
+        />
+        <button onClick={changeSearch}>Cari</button>
       </div>
       <small>
         Ditemukan {props.resultCount} data dengan pencarian kata "{search}"
