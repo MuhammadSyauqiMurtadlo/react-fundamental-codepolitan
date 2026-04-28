a5fdfbef22afdbe26b910d0dd57ff1af1d4b5e12
Syauqi <sauki084@gmail.com>
2026-04-29 05:41:12 +0700

    Conditional Rendering 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import "./App.css";
 import Homepage from "./pages/Index";
 
 function App() {
-  const value = false;
   return (
     <>
       <div className="App">
-        {value ? "benar" : "salah"}
         <Homepage />
       </div>
     </>
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,24 @@
+const ArticleStatus = ({ isNew }) => {
+  return isNew ? (
+    <span style={{ color: "red" }}> Terbaru </span>
+  ) : (
+    <span style={{ color: "gray" }}> Lama </span>
+  );
+};
+
+// const NewArticle = () => {
+//   return <span> BARUUU POOLLL </span>;
+// };
+
 function Article(props) {
   return (
     <>
       <h3>{props.title}</h3>
       <small>
         Date : {props.date}, Tags : {props.tags.join(", ")}
+        {""}
+        <ArticleStatus isNew={props.isNew} />
+        {/* {props.isNew && <NewArticle />} */}
       </small>
     </>
   );
